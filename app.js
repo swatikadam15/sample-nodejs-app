31da7df47cf5c23c6b65d742e8c9db0e748b54ee
swatikadam15 <swatikadam169@gmail.com>
2026-05-20 18:02:42 +0000

    updaated js file

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('DevOps CI/CD Pipeline Working Successfully');
+    res.send('DevOps CI/CD Pipeline Working Successfully swati');
 });
 
 app.listen(3000, () => {
-    console.log('Application running on port 30');
+    console.log('Application running on port 30000');
 });
