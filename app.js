f83647edbdb12a3cee6f0388500b5d29717abe41
swatikadam15 <swatikadam169@gmail.com>
2026-05-20 09:07:54 +0000

    node.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('DevOps CI/CD Pipeline Working Successfully');
+});
+
+app.listen(3000, () => {
+    console.log('Application running on port 3000');
+});
