a62c9eaf1e8368b7edf5fd3a2a024e4795ac477a
swatikadam15 <swatikadam169@gmail.com>
2026-05-21 07:11:54 +0000

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('DevOps CI/CD Pipeline Working Successfully swati');
+    res.send('DevOps CI/CD Pipeline Working Successfully');
 });
 
 app.listen(3000, () => {
